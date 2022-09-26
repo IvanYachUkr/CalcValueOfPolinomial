66fb464cca1dafd671cf41d48e8e5e08ade6b600
IvanYachUkr <vanya.yach2004@gmail.com>
2022-09-26 16:23:06 +0300

    find sum of generated even numbers and print it

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main() {
 
     int min = RAND_MAX;
     int max = 0;
+    int sum_of_even_nums = 0;
     for (int i = 0; i < num_of_rand_nums; i++) {
         int rand_num = rand();
         std::cout << rand_num<< "\n";
@@ -20,9 +21,13 @@ int main() {
             min = rand_num;
         }
 
+        if (rand_num%2 == 0){
+            sum_of_even_nums += rand_num;
+        }
     }
     std::cout << "Maximum generated value is " << max << "\n";
     std::cout << "Minimum generated value is " << min << "\n";
+    std::cout << "Sum of generated even numbers is " << sum_of_even_nums << "\n";
 
 
 
