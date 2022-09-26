a92d5ee2f802ea88002ead69f6f6350f6442f9d5
IvanYachUkr <vanya.yach2004@gmail.com>
2022-09-26 16:11:11 +0300

    generate sequence of random numbers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    srand(time(nullptr));
+
+    int num_of_rand_nums;
+    std::cout << "How many random numbers would you like to generate? : ";
+    std::cin >> num_of_rand_nums;
+
+
+    for (int i = 0; i < num_of_rand_nums; i++) {
+        int rand_num = rand();
+        std::cout << rand_num<< "\n";
+    }
+
+
     return 0;
 }
